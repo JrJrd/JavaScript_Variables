ea3320036587148ee62be05b9658223044f5f3fa
seaer <jrd.seaer@gmail.com>
2023-08-30 02:15:38 +0300

    just js

--- app.js
+++ app.js
@@ -4,7 +4,31 @@ console.log(
 );
 
 // Exercise 1
-
+let lastName = "j";
+var age = 30;
 // Exercise 2
+const firstName= "Saer";
 
+lastName="jay";
+age = 27;
 // Exercise 3
+let lang = "js";
+let createdYear = 1995;
+let isCaseSensitive = true;
+// Exercise 4
+let price = 19.99;
+let isOnSale = false;
+let salePercentage = 15;
+let stock = 0;
+let inStock = false;
+let selectedSize = "M";
+
+// Exercise 5
+
+let title = "Name of the Wind";
+let author = "Patrick Rothfuss";
+let pageCount = 722;
+let bookmark = 456;
+let hasRead = true;
+
+console.log(`${title} by ${author} is ${pageCount} pages long. Your last bookmark was page ${bookmark}.`)
